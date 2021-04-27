2bf851f6bd89366a976d427ef3751347fe81daca
rrajchal <rrajchal@gmail.com>
2021-04-26 22:37:07 -0600

    Some enhancement
    Added a few project and updated resume

---
+++ js/temperature.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {    
+    var apiKey = "232fdb488171fce391dae73eeadbab38";   //   e790d4e82006ca50828418f49c2be10c
+    console.log('state_info is: ${state_info}');
+    
+    function tempToColor(temperature) {
+        switch(true) {
+          case (temperature <= 10):
+            return "blue";
+            break;
+          case (temperature > 10 && temperature <= 30):
+            return "cyan";
+            break;
+          case (temperature > 30 && temperature <= 50):
+            return "green";
+            break;
+          case (temperature > 50 && temperature <= 80):
+            return "orange";
+            break;
+          case (temperature > 80):
+            return "red";
+            break;
+          default:
+            return "gray";
+        }
+    }
+    
+Object.keys(state_info).forEach(function(key) {
+    
+    let stateName = key;
+    let stateLat = state_info[key]['lat'];
+    let stateLng = state_info[key]['lng'];
+    let url =`https://api.darksky.net/forecast/${apiKey}/${stateLat},${stateLng}`;
+
+    setTimeout(function() {
+        getStateTemp(url).then(function(tempres) {
+            setStateColor(stateName, tempToColor(tempres));
+        });
+    }, 100);
+});
+    
+    function setStateColor(state, color) {
+        console.log("Setting " + state + " to " + color);
+        $('#'+state).css('fill', color);
+    }
+
+    
+    function getStateTemp(url) {
+        return new Promise((resolve, reject) => {
+
+            $.ajax({url:url, dataType:"jsonp"}).then(function(data) {
+                        resolve(data.currently.temperature); 
+            });  
+        });  
+    }
+});
--- js/tictac.js
+++ js/tictac.js
@@ -1,13 +1,3 @@
-// define functions
-var hello_world = function() {
-  console.log('hello world');
-};
-
-// when jquery is loaded and document is ready
-$(document).ready(function() {
-  // initialize functions
-  hello_world();
-});
 
 /*
 Code Written by Cory Fogliani (Email: cory@ijustdontcare.com)
@@ -17,265 +7,161 @@ For this and over 400+ free scripts, visit http://javascriptkit.com
 */
 
 //if IE4/NS6, apply style
-if (document.all||document.getElementById){
-document.write('<style>.tictac{')
-document.write('width:80px;height:80px;')
-document.write('}</style>')
+if (document.all || document.getElementById){
+	document.write('<style>.tictac{'); 
+	document.write('width:80px;height:80px;'); 
+	document.write('}</style>'); 
 }
 
-var sqr1
-var sqr2
-var sqr3
-var sqr4
-var sqr5
-var sqr6
-var sqr7
-var sqr8
-var sqr9
-var sqr1T = 0
-var sqr2T = 0
-var sqr3T = 0
-var sqr4T = 0
-var sqr5T = 0
-var sqr6T = 0
-var sqr7T = 0
-var sqr8T = 0
-var sqr9T = 0
-var moveCount = 0
-var turn = 0
-var mode = 1
+var sqr1; 
+var sqr2; 
+var sqr3; 
+var sqr4; 
+var sqr5; 
+var sqr6; 
+var sqr7; 
+var sqr8; 
+var sqr9; 
+var sqr1T = 0; 
+var sqr2T = 0; 
+var sqr3T = 0; 
+var sqr4T = 0; 
+var sqr5T = 0; 
+var sqr6T = 0; 
+var sqr7T = 0; 
+var sqr8T = 0; 
+var sqr9T = 0; 
+var moveCount = 0; 
+var turn = 0; 
+var mode = 1; 
 
 function vari()
 {
-sqr1 = document.tic.sqr1.value
-sqr2 = document.tic.sqr2.value
-sqr3 = document.tic.sqr3.value
-sqr4 = document.tic.sqr4.value
-sqr5 = document.tic.sqr5.value
-sqr6 = document.tic.sqr6.value
-sqr7 = document.tic.sqr7.value
-sqr8 = document.tic.sqr8.value
-sqr9 = document.tic.sqr9.value
+	sqr1 = document.tic.sqr1.value; 
+	sqr2 = document.tic.sqr2.value; 
+	sqr3 = document.tic.sqr3.value; 
+	sqr4 = document.tic.sqr4.value; 
+	sqr5 = document.tic.sqr5.value; 
+	sqr6 = document.tic.sqr6.value; 
+	sqr7 = document.tic.sqr7.value; 
+	sqr8 = document.tic.sqr8.value; 
+	sqr9 = document.tic.sqr9.value; 
 }
+
 function check()
 {
   if(sqr1 == " X " && sqr2 == " X " && sqr3 == " X ")
   {
-    alert("You Win!")
-    reset()
+    alert("You Win!"); 
+    reset(); 
   } 
   else if(sqr4 == " X " && sqr5 == " X " && sqr6 == " X ")
   {
-    alert("You Win!")
-    reset()
+    alert("You Win!"); 
+    reset(); 
   } 
   else if(sqr7 == " X " && sqr8 == " X " && sqr9 == " X ")
   {
-    alert("You Win!")
-    reset()
+    alert("You Win!"); 
+    reset(); 
   }
   else if(sqr1 == " X " && sqr5 == " X " && sqr9 == " X ")
   {
-    alert("You Win!")
-    reset()
+    alert("You Win!"); 
+    reset(); 
   }
   else if(sqr1 == " X " && sqr4 == " X " && sqr7 == " X ")
   {
-    alert("You Win!")
-    reset()
+    alert("You Win!"); 
+    reset(); 
   }
   else if(sqr2 == " X " && sqr5 == " X " && sqr8 == " X ")
   {
-    alert("You Win!")
-    reset()
+    alert("You Win!"); 
+    reset(); 
   }
   else if(sqr3 == " X " && sqr6 == " X " && sqr9 == " X ")
   {
-    alert("You Win!")
-    reset()
+    alert("You Win!"); 
+    reset(); 
   }
   else if(sqr1 == " X " && sqr5 == " X " && sqr9 == " X ")
   {
-    alert("You Win!")
-    reset()
+    alert("You Win!"); 
+    reset(); 
   }
   else if(sqr3 == " X " && sqr5 == " X " && sqr7 == " X ")
   {
-    alert("You Win!")
-    reset()
+    alert("You Win!"); 
+    reset(); 
   }
   else
   {
-    winCheck()
-    check2()
-    drawCheck()  
+    winCheck(); 
+    check2(); 
+    drawCheck(); 
   } 
 }
 
 function check2()
 {
-  vari()
-  drawCheck()
   if(sqr1 == " O " && sqr2 == " O " && sqr3 == " O ")
   {
-    alert("You Lose!")
-    reset()
+    alert("You Lose!"); 
+    reset(); 
   } 
   else if(sqr4 == " O " && sqr5 == " O " && sqr6 == " O ")
   {
-    alert("You Lose!")
-    reset()
+    alert("You Lose!"); 
+    reset(); 
   } 
   else if(sqr7 == " O " && sqr8 == " O " && sqr9 == " O ")
   {
-    alert("You Lose!")
-    reset()
+    alert("You Lose!"); 
+    reset(); 
   }
   else if(sqr1 == " O " && sqr5 == " O " && sqr9 == " O ")
   {
-    alert("You Lose!")
-    reset()
+    alert("You Lose!"); 
+    reset(); 
   }
   else if(sqr1 == " O " && sqr4 == " O " && sqr7 == " O ")
   {
-    alert("You Lose!")
-    reset()
+    alert("You Lose!"); 
+    reset(); 
   }
   else if(sqr2 == " O " && sqr5 == " O " && sqr8 == " O ")
   {
-    alert("You Lose!")
-    reset()
+    alert("You Lose!"); 
+    reset(); 
   }
   else if(sqr3 == " O " && sqr6 == " O " && sqr9 == " O ")
   {
-    alert("You Lose!")
-    reset()
+    alert("You Lose!"); 
+    reset(); 
   }
   else if(sqr1 == " O " && sqr5 == " O " && sqr9 == " O ")
   {
-    alert("You Lose!")
-    reset()
+    alert("You Lose!"); 
+    reset(); 
   }
   else if(sqr3 == " O " && sqr5 == " O " && sqr7 == " O ")
   {
-    alert("You Lose!")
-    reset()
+    alert("You Lose!"); 
+    reset(); 
   }
-}
-
-function player1Check()
-{
-  if(sqr1 == " X " && sqr2 == " X " && sqr3 == " X ")
-  {
-    alert("Player 1 wins!")
-    reset()
-  } 
-  else if(sqr4 == " X " && sqr5 == " X " && sqr6 == " X ")
-  {
-    alert("Player 1 wins!")
-    reset()
-  } 
-  else if(sqr7 == " X " && sqr8 == " X " && sqr9 == " X ")
-  {
-    alert("Player 1 wins!")
-    reset()
-  }
-  else if(sqr1 == " X " && sqr5 == " X " && sqr9 == " X ")
-  {
-    alert("Player 1 wins!")
-    reset()
-  }
-  else if(sqr1 == " X " && sqr4 == " X " && sqr7 == " X ")
-  {
-    alert("Player 1 wins!")
-    reset()
-  }
-  else if(sqr2 == " X " && sqr5 == " X " && sqr8 == " X ")
-  {
-    alert("Player 1 wins!")
-    reset()
-  }
-  else if(sqr3 == " X " && sqr6 == " X " && sqr9 == " X ")
-  {
-    alert("Player 1 wins!")
-    reset()
-  }
-  else if(sqr1 == " X " && sqr5 == " X " && sqr9 == " X ")
-  {
-    alert("Player 1 wins!")
-    reset()
-  }
-  else if(sqr3 == " X " && sqr5 == " X " && sqr7 == " X ")
-  {
-    alert("Player 1 wins!")
-    reset()
-  }
-  else
-  {
-    player2Check()
-    drawCheck()  
-  } 
-}
-
-function player2Check()
-{
-  vari()
-  drawCheck()
-  if(sqr1 == " O " && sqr2 == " O " && sqr3 == " O ")
-  {
-    alert("Player 2 wins!")
-    reset()
-  } 
-  else if(sqr4 == " O " && sqr5 == " O " && sqr6 == " O ")
-  {
-    alert("Player 2 wins!")
-    reset()
-  } 
-  else if(sqr7 == " O " && sqr8 == " O " && sqr9 == " O ")
-  {
-    alert("Player 2 wins!")
-    reset()
-  }
-  else if(sqr1 == " O " && sqr5 == " O " && sqr9 == " O ")
-  {
-    alert("Player 2 wins!")
-    reset()
-  }
-  else if(sqr1 == " O " && sqr4 == " O " && sqr7 == " O ")
-  {
-    alert("Player 2 wins!")
-    reset()
-  }
-  else if(sqr2 == " O " && sqr5 == " O " && sqr8 == " O ")
-  {
-    alert("Player 2 wins!")
-    reset()
-  }
-  else if(sqr3 == " O " && sqr6 == " O " && sqr9 == " O ")
-  {
-    alert("Player 2 wins!")
-    reset()
-  }
-  else if(sqr1 == " O " && sqr5 == " O " && sqr9 == " O ")
-  {
-    alert("Player 2 wins!")
-    reset()
-  }
-  else if(sqr3 == " O " && sqr5 == " O " && sqr7 == " O ")
-  {
-    alert("Player 2 wins!")
-    reset()
-  }
+  vari(); 
+  drawCheck(); 
 }
 
 function drawCheck()
 {
-  vari()
-  moveCount = sqr1T + sqr2T + sqr3T + sqr4T + sqr5T + sqr6T + sqr7T + sqr8T + sqr9T 
+  vari(); 
+  moveCount = sqr1T + sqr2T + sqr3T + sqr4T + sqr5T + sqr6T + sqr7T + sqr8T + sqr9T ; 
   if(moveCount == 9)
   {
-    reset()
-    alert("Draw") 
+    reset(); 
+    alert("Draw") ; 
   }
 }
 
@@ -284,7 +170,7 @@ function winCheck()
   
   if(sqr1 == " O " && sqr2 == " O " && sqr3T == 0 && turn == 1)
   {
-    document.tic.sqr3.value = " O "
+    document.tic.sqr3.value = " O "; 
     sqr3T = 1;
     turn = 0;
   }
@@ -438,6 +324,7 @@ function winCheck()
   }
   check2()
 }
+
 function computer()
 {
   
@@ -593,97 +480,111 @@ function computer()
   }
   else
   {
-    AI()
+    AI(); 
   }
-  check2()
+  check2(); 
 }
 
 function AI()
 {
   if(document.tic.sqr5.value == "     " && turn == 1)
   {
-    document.tic.sqr5.value = " O "
-    turn = 0
-    sqr5T = 1
+    document.tic.sqr5.value = " O "; 
+    turn = 0; 
+    sqr5T = 1; 
   }
   else if(document.tic.sqr1.value == "     " && turn == 1)
   {
-    document.tic.sqr1.value = " O "
-    turn = 0
-    sqr1T = 1
+    document.tic.sqr1.value = " O "; 
+    turn = 0; 
+    sqr1T = 1; 
   }
   else if(document.tic.sqr9.value == "     " && turn == 1)
   {
-    document.tic.sqr9.value = " O "
-    turn = 0
-    sqr9T = 1
+    document.tic.sqr9.value = " O "; 
+    turn = 0; 
+    sqr9T = 1; 
   }
   else if(document.tic.sqr6.value == "     " && turn == 1)
   {
-    document.tic.sqr6.value = " O "
-    turn = 0
-    sqr6T = 1
+    document.tic.sqr6.value = " O ";
+    turn = 0;
+    sqr6T = 1;
   }
   else if(document.tic.sqr2.value == "     " && turn == 1)
   {
-    document.tic.sqr2.value = " O "
-    turn = 0
-    sqr2T = 1
+    document.tic.sqr2.value = " O ";
+    turn = 0;
+    sqr2T = 1;
   }
   else if(document.tic.sqr8.value == "     " && turn == 1)
   {
-    document.tic.sqr8.value = " O "
-    turn = 0
-    sqr8T = 1
+    document.tic.sqr8.value = " O ";
+    turn = 0;
+    sqr8T = 1;
   }
   else if(document.tic.sqr3.value == "     " && turn == 1)
   {
-    document.tic.sqr3.value = " O "
-    turn = 0
-    sqr3T = 1
+    document.tic.sqr3.value = " O ";
+    turn = 0;
+    sqr3T = 1;
   }
   else if(document.tic.sqr7.value == "     " && turn == 1)
   {
-    document.tic.sqr7.value = " O "
-    turn = 0
-    sqr7T = 1
+    document.tic.sqr7.value = " O ";
+    turn = 0;
+    sqr7T = 1;
   }
   else if(document.tic.sqr4.value == "     " && turn == 1)
   {
-    document.tic.sqr4.value = " O "
-    turn = 0
-    sqr4T = 1
+    document.tic.sqr4.value = " O ";
+    turn = 0;
+    sqr4T = 1;
   }
-  vari()
-  check2()
+  vari();
+  check2();
 }
 
 function reset()
 {
-  document.tic.sqr1.value = "     "
-  document.tic.sqr2.value = "     "
-  document.tic.sqr3.value = "     "
-  document.tic.sqr4.value = "     "
-  document.tic.sqr5.value = "     "
-  document.tic.sqr6.value = "     "
-  document.tic.sqr7.value = "     "
-  document.tic.sqr8.value = "     "
-  document.tic.sqr9.value = "     "
-  sqr1T = 0
-  sqr2T = 0
-  sqr3T = 0
-  sqr4T = 0
-  sqr5T = 0
-  sqr6T = 0
-  sqr7T = 0
-  sqr8T = 0
-  sqr9T = 0
-  vari()
-  turn = 0
-  moveCount = 0
+  
+  document.tic.sqr1.value = "     ";
+  document.tic.sqr2.value = "     ";
+  document.tic.sqr3.value = "     ";
+  document.tic.sqr4.value = "     ";
+  document.tic.sqr5.value = "     ";
+  document.tic.sqr6.value = "     ";
+  document.tic.sqr7.value = "     ";
+  document.tic.sqr8.value = "     ";
+  document.tic.sqr9.value = "     ";
+  sqr1T = 0;
+  sqr2T = 0;
+  sqr3T = 0;
+  sqr4T = 0;
+  sqr5T = 0;
+  sqr6T = 0;
+  sqr7T = 0;
+  sqr8T = 0;
+  sqr9T = 0;
+  vari();
+  turn = 0;
+  moveCount = 0;
 }
 
-function resetter()
-{
-  reset()
+function sleep(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
 }
+
+async function demo() {
+  console.log('Taking a break...');
+  await sleep(2000);
+  console.log('Two seconds later, showing sleep in a loop...');
+
+  // Sleep in loop
+  for (let i = 0; i < 5; i++) {
+    if (i === 3)
+      await sleep(2000);
+    console.log(i);
+  }
+}
+

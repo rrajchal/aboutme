762970a71cc11132f41407fce605c6f338649017
rrajchal <rrajchal@gmail.com>
2021-04-26 23:30:44 -0600

    Bug fixes

--- js/temperature.js
+++ js/temperature.js
@@ -1,30 +1,31 @@
-  
-var apiKey = "e790d4e82006ca50828418f49c2be10c"; 
-console.log('state_info is: ${state_info}');
+$(document).ready(function() {    
+    var apiKey = "e790d4e82006ca50828418f49c2be10c"; 
+    console.log('state_info is: ${state_info}');
     
-function tempToColor(temperature) {
-	switch(true) {
-	  case (temperature <= 10):
-		return "blue";
-		break;
-	  case (temperature > 10 && temperature <= 30):
-		return "cyan";
-		break;
-	  case (temperature > 30 && temperature <= 50):
-		return "green";
-		break;
-	  case (temperature > 50 && temperature <= 80):
-		return "orange";
-		break;
-	  case (temperature > 80):
-		return "red";
-		break;
-	  default:
-		return "gray";
-	}
-}
+    function tempToColor(temperature) {
+        switch(true) {
+          case (temperature <= 10):
+            return "blue";
+            break;
+          case (temperature > 10 && temperature <= 30):
+            return "cyan";
+            break;
+          case (temperature > 30 && temperature <= 50):
+            return "green";
+            break;
+          case (temperature > 50 && temperature <= 80):
+            return "orange";
+            break;
+          case (temperature > 80):
+            return "red";
+            break;
+          default:
+            return "gray";
+        }
+    }
     
 Object.keys(state_info).forEach(function(key) {
+    
     let stateName = key;
     let stateLat = state_info[key]['lat'];
     let stateLng = state_info[key]['lng'];
@@ -37,16 +38,18 @@ Object.keys(state_info).forEach(function(key) {
     }, 100);
 });
     
-function setStateColor(state, color) {
-	console.log("Setting " + state + " to " + color);
-	$('#'+state).css('fill', color);
-}
+    function setStateColor(state, color) {
+        console.log("Setting " + state + " to " + color);
+        $('#'+state).css('fill', color);
+    }
 
+    
+    function getStateTemp(url) {
+        return new Promise((resolve, reject) => {
 
-function getStateTemp(url) {
-	return new Promise((resolve, reject) => {
-		$.ajax({url:url, dataType:"jsonp"}).then(function(data) {
-					resolve(data.currently.temperature); 
-		});  
-	});  
-}
+            $.ajax({url:url, dataType:"jsonp"}).then(function(data) {
+                        resolve(data.currently.temperature); 
+            });  
+        });  
+    }
+});
